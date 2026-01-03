edbef45b4ab9fbdf61cdc9712b840246c3b00f8e
Dima Borovinski <borovinskijdmitrij178@gmail.com>
2026-01-03 05:02:49 +0300

    Test

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func throwPipe(score *int) int {
+	fmt.Println("Ваш счет:", *score)
+	fmt.Println("Пролетели через трубу")
+	*score++
+	fmt.Println("Ваш счет:", *score)
+	goodScore(score)
+	return *score
+}
+
+func goodScore(score *int) {
+	if *score > 10 {
+		fmt.Println("Хороший результат!")
+	} else {
+		fmt.Println("Плохой результат!")
+	}
+}
+
+func breakThrow(score *int) int {
+	fmt.Println("Врезались в трубу")
+	*score = 0
+	return *score
+}
+
+func main() {
+	const (
+		textReady string = "Get Ready"
+		gameOver  string = "Game Over"
+	)
+	var score int = 15
+	x := throwPipe(&score)
+	fmt.Println(x)
+
+}
